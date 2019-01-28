c4ad6ddb7a513ebdcd6d46eaa54170a8e72c4468
flawlor <flawlor@guidewire.com>
2019-01-28 13:19:02 +0000

    Added backend code

---
+++ checklist-backend/src/main/java/com/checklist/model/TestCase.java
@@ -0,0 +1,35 @@
+package com.checklist.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "testcase")
+public class TestCase {
+    @Id
+    @GeneratedValue(generator = "testcase_generator")
+    @SequenceGenerator(
+            name = "testcase_generator",
+            sequenceName = "testcase_sequence",
+            initialValue = 1
+    )
+    private Long id;
+    private String name;
+    private String description;
+    private String comment;
+    private int status;
+    private String epicId;
+
+    TestCase() {}
+
+    TestCase(Long id, String  name, String description, String comment, int status, String epicId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.comment = comment;
+        this.status = status;
+        this.epicId = epicId;
+    }
+}
