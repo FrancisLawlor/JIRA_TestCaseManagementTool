869b3f1c4251f5c8b9522284f2fc25b13ff84bd2
Odhran Daly <odaly@odaly-mbp15.guidewire.com>
2019-01-29 15:27:03 +0000

    First stage

--- checklist-management/src/controller/ProjectController.js
+++ checklist-management/src/controller/ProjectController.js
@@ -1,4 +1,10 @@
 import React, {Component} from "react";
+import { Grid } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Thumbnail } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
 
 class ProjectCard extends Component {
     constructor(props) {
@@ -7,7 +13,22 @@ class ProjectCard extends Component {
 
     render() {
         // callback = this.props.callback(project.id)
-        return <li> project card goes here ... </li>
+        return (
+            <div className="grid-item" bsStyle="sm">
+            
+           
+       
+             <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200" className="card">
+                 <h3>Thumbnail label</h3>
+                 <p>Description</p>
+                 <p>
+                 <Button bsStyle="primary">Button</Button>&nbsp;
+                 <Button bsStyle="default">Button</Button>
+                 </p>
+             </Thumbnail>
+            </div>
+            
+        )
     }
 }
 
@@ -18,12 +39,12 @@ class ProjectList extends Component {
     }
 
     render() {
-        return <ul>{
+        return <div className="grid-container">{
             this.state.data.map((project) => {
                 return <ProjectCard key={project.id} project={project} callback={this.props.callback} />
                 // return <li key={project.id} onClick={() => this.props.callback(project.id)}><img src={project.avatarUrls["48x48"]}/>{project.name}</li>
             })
-        }</ul>
+        }</div>
     }
 
     componentDidMount() {
@@ -32,7 +53,7 @@ class ProjectList extends Component {
         //     .then(data => this.setState({data}));
 
         this.setState({
-            data : [1,2,3]
+            data : [1,2,3, 4, 5, 6]
         })
     }
 }
