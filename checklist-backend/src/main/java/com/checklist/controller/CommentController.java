87faee23179d6b2b78b767fdf2659928bb35396f
Erol Selitektay <eselitektay@guidewire.com>
2019-01-29 21:42:25 +0000

    added comment end-point

---
+++ checklist-backend/src/main/java/com/checklist/controller/CommentController.java
@@ -0,0 +1,30 @@
+package com.checklist.controller;
+
+import com.checklist.model.Comment;
+import com.jayway.jsonpath.JsonPath;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.validation.Valid;
+
+
+@RestController
+public class CommentController {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+
+    @PostMapping (path = "/comments/{id}")
+    public void createComment(@PathVariable String id, @Valid @RequestBody String comment) {
+        HttpHeaders requestHeaders = new HttpHeaders();
+        String commentJustBody = JsonPath.parse(comment).read("$.body");
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+//        TODO read token from config file
+        requestHeaders.add("Authorization", "Basic <Auth Token>");
+        Comment commentBody = new Comment(commentJustBody);
+        String url = "https://guidewirejira.atlassian.net/rest/api/2/issue/" + id + "/comment";
+        HttpEntity<Comment> requestEntity = new HttpEntity<>(commentBody, requestHeaders);
+        restTemplate.exchange(url, HttpMethod.POST, requestEntity, Comment.class);
+    }
+}
